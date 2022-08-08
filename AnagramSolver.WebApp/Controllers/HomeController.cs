765d25495074eb67a07584f3eaf84ef7c4e0053c
Eimantas Šileikis <eimantas.sileikis@visma.com>
2022-08-08 13:15:25 +0300

    Added MVC project and view with anagrams

---
+++ AnagramSolver.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using AnagramSolver.Contracts.Interfaces;
+using AnagramSolver.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AnagramSolver.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IAnagramSolver _anagramSolver;
+
+        public HomeController(ILogger<HomeController> logger, IAnagramSolver anagramSolver)
+        {
+            _logger = logger;
+            _anagramSolver = anagramSolver;
+        }
+
+        public IActionResult Index(string word)
+        {
+            if (word == null || word == string.Empty)
+                return new EmptyResult();
+
+            var data = _anagramSolver.GetAnagrams(word);
+
+            return View(data);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
